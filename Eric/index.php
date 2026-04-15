d1d03487ae950cb32f01f129afaec22e6c43398b
ERICH <niyitangaeric77@gmail.com>
2026-04-15 04:13:58 -0700

    Update index.php

--- Eric/index.php
+++ Eric/index.php
@@ -1,31 +1,4 @@
 
-<?php
-session_start();
-//database connection
-$server='localhost';
-$user='root';
-$password='';
-$database='portifolio';
-$conn=new mysqli($server,$user,$password,$database);
-
-
-
-if(isset($_POST['submit'])){
-    $name=$_POST['full'];
-      $email=$_POST['email'];
-        $message=$_POST['message'];
-//sending data to the database
-$sql="INSERT INTO messages(username,email,message)VALUES('$name','$email','$message')";
-$insert=$conn->query($sql);
-if($insert){
-    ?>
-    
-  <?php
-
-}
-
-}
-?>
 
 
 
